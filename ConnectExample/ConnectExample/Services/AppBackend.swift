3082d34328b1899847d8d8d9d5a05aca27d2c597
Stefan Becker <586690+snacker81@users.noreply.github.com>
2025-07-18 16:38:35 +0200

    More cleanup

--- ConnectExample/ConnectExample/Services/AppBackend.swift
+++ ConnectExample/ConnectExample/Services/AppBackend.swift
@@ -12,6 +12,7 @@ import AWSCognitoAuthPlugin
 import AWSPluginsCore
 
 class AppBackend {
+    /// Get connectToken from backend (which gets it from Corbado Backend API)
     static func getConnectToken(connectTokenType: ConnectTokenType, idToken: String) async throws -> String {
         // 1. Set up request
         let urlString = "https://feature-wv.connect-next.playground.corbado.io/connectToken"
@@ -23,7 +24,7 @@ class AppBackend {
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
-        let requestPayload = ConnectTokenRequest(
+        let requestPayload = GetConnectTokenRequest(
             connectTokenType: connectTokenType.rawValue,
             idToken: idToken
         )
@@ -50,8 +51,59 @@ class AppBackend {
 
         // 4. Decode the JSON response
         do {
-            let decodedResponse = try JSONDecoder().decode(ConnectTokenResponse.self, from: data)
+            let decodedResponse = try JSONDecoder().decode(GetConnectTokenResponse.self, from: data)
             return decodedResponse.token
+        } catch {
+            print("Error decoding JSON response: \(error)")
+            
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("Raw response string: \(responseString)")
+            }
+            
+            throw error
+        }
+    }
+    
+    /// Verify signedPasskeyData with backend (which verifies with Corbado Backend API)
+    static func verifySignedPasskeyData(signedPasskeyData: String) async throws -> (Bool, String) {
+        // 1. Set up request
+        let urlString = "https://<your-backend>/auth/verifySignedPasskeyData"
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let requestPayload = VerifySignedPasskeyDataRequest(
+            signedPasskeyData: signedPasskeyData
+        )
+        
+        do {
+            let jsonData = try JSONEncoder().encode(requestPayload)
+            request.httpBody = jsonData
+        } catch {
+            throw error
+        }
+
+        // 2. Perform request
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        // 3. Check response
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
+            print("Error: HTTP Status Code \(statusCode)")
+            
+            // You might want to try decoding error details from 'data' here if your API
+            // provides them or a custom error
+            throw URLError(.badServerResponse)
+        }
+
+        // 4. Decode the JSON response
+        do {
+            let decodedResponse = try JSONDecoder().decode(VerifySignedPasskeyDataResponse.self, from: data)
+            return (decodedResponse.success, decodedResponse.sessionId)
         } catch {
             print("Error decoding JSON response: \(error)")
             
@@ -78,11 +130,20 @@ class AppBackend {
     }
 }
 
-struct ConnectTokenRequest: Codable {
+struct GetConnectTokenRequest: Codable {
     let connectTokenType: String
     let idToken: String
 }
 
-struct ConnectTokenResponse: Codable {
+struct GetConnectTokenResponse: Codable {
     let token: String
+}
+
+struct VerifySignedPasskeyDataRequest: Codable {
+    let signedPasskeyData: String
+}
+
+struct VerifySignedPasskeyDataResponse: Codable {
+    let success: Bool
+    let sessionId: String
 }
